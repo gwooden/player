126f2c582c2aa97283bb95ae64b2b99b2394cc28
Gerry Wooden <gwooden@users.noreply.github.com>
2022-04-28 18:29:32 -0400

    Add files via upload
    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from typing import List,Optional
+from fastapi import FastAPI
+
+from db.config import engine, Base, async_session
+from db.dals.player_dal import PlayerDAL
+from db.models.player import Player
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    # create db tables
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+@app.post("/players")
+async def create_player(firstName: str, lastName: str, number: int):
+    async with async_session() as session:
+        async with session.begin():
+            player_dal = PlayerDAL(session)
+            return await player_dal.create_player(firstName, lastName, number)
+
+@app.get("/players")
+async def get_all_players() -> List[Player]:
+    async with async_session() as session:
+        async with session.begin():
+            player_dal = PlayerDAL(session)
+            return await player_dal.get_all_players()
+
+@app.put("/players/{player_id}")
+async def update_player(player_id: int, firstName: Optional[str] = None, lastName: Optional[str] = None, number: Optional[int] = None):
+    async with async_session() as session:
+        async with session.begin():
+            player_dal = PlayerDAL(session)
+            return await player_dal.update_player(player_id, firstName, lastName, number)
